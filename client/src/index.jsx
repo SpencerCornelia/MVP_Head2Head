596cb4ad24210f017e958646ff448bc911a10c93
Spencer <spencer0cornelia@gmail.com>
2018-02-03 14:16:53 -0800

    working ajax get request to get nfl games

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,12 +6,33 @@ import Games from './components/Games.jsx';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      games: []
+    }
+    this.getGames();
+  }
+
+  getGames() {
+    $.ajax({
+      url: 'http://localhost:3000/nflGames',
+      method: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log("data =", data)
+        this.setState({
+          games: data
+        });
+      }.bind(this),
+      error: function(err) {
+        console.log("error in get request to repos. err =", err);
+      }
+    });
   }
 
   render() {
     return (<div>
         <h1>Head 2 Head Sports Gambling</h1>
-        <Games />
+        <Games games={this.state.games}/>
       </div>
     );
   }
