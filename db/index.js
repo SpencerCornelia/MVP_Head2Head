4a5e8467b1c1fdc5de19b20c1fdf7f64cf96eaab
Spencer <spencer0cornelia@gmail.com>
2018-02-03 14:16:39 -0800

    added function to get all nfl games and return to helper

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect().then(
+mongoose.connect('mongodb://localhost/head2head').then(
   () => { console.log("successful DB connection") },
   err => { console.log("error with DB connection") }
 );
@@ -29,3 +29,29 @@ let userSchema = mongoose.Schema({
 });
 
 let User = mongoose.model('User', userSchema);
+
+let allNFLGames = function(cb) {
+  Game.find({sport: 'NFL'}).exec(cb);
+};
+
+// use this to create games for test data
+// let saveGame = function(body) {
+//   var newGame = new Game({
+//     sport: 'NFL',
+//     away_team: body.away_team,
+//     home_team: body.home_team,
+//     away_spread: body.away_line,
+//     home_spread: body.home_line,
+//     away_id: body.away_team_id,
+//     home_id: body.home_team_id
+//   });
+
+//   newGame.save((err) => {
+//     if (err) {
+//       console.log('error saving newGame to db')
+//     }
+//   })
+// }
+
+// module.exports.saveGame = saveGame;
+module.exports.allNFLGames = allNFLGames;
