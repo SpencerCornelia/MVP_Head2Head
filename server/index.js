c87b92ac702a21a7e6b88f056258d100ac8fe5e8
Spencer <spencer0cornelia@gmail.com>
2018-02-03 14:16:23 -0800

    added route to get nflGames

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let app = express();
+const gameRetrieval = require('../helpers/gameretrieval.js');
+
+// use this for testing/adding initial data to db
+const db = require('../db/index.js');
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/createGame', function(req, res) {
+  db.saveGame(req.body);
+});
+
+app.get('/nflGames', function(req, res) {
+  gameRetrieval.retrieveNFLGames((games) => {
+    res.json(games);
+  });
+});
 
 let port = 3000;
 app.listen(port, function() {
