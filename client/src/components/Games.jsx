5d9d358bc56761b2eaeef2871469214a95e60c89
Spencer <spencer0cornelia@gmail.com>
2018-02-03 14:17:36 -0800

    added rendering for games

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -8,6 +8,19 @@ class Games extends React.Component {
   render() {
     return (<div>
       <h4>Today's Games</h4>
+      {this.props.games.map((game) =>
+        <div key={game._id}>
+          <div>{game.away_team} vs. {game.home_team}</div>
+          <button>$20 {game.away_spread}</button>
+          <button>$20 {game.home_spread}</button><br/>
+          <button>$40 {game.away_spread}</button>
+          <button>$40 {game.home_spread}</button><br/>
+          <button>$100 {game.away_spread}</button>
+          <button>$100 {game.home_spread}</button><br/>
+          <button>$250 {game.away_spread}</button>
+          <button>$250 {game.home_spread}</button><br/>
+        </div>
+      )}
     </div>)
   }
 }
