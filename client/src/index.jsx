192878b97df4ae0898cc773e260d7605560ec28e
Spencer <spencer0cornelia@gmail.com>
2018-02-04 11:09:54 -0800

    moved everything to App component. index.jsx now renders app component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
-import Games from './components/Games.jsx';
+import { Router, Route, browserHistory } from 'react-router';
+import { Switch, BrowserRouter } from 'react-router-dom';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      games: []
-    }
-    this.getGames();
-  }
+import App from './components/App.jsx';
+import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
-  getGames() {
-    $.ajax({
-      url: 'http://localhost:3000/nflGames',
-      method: 'GET',
-      contentType: 'application/json',
-      success: function(data) {
-        console.log("data =", data)
-        this.setState({
-          games: data
-        });
-      }.bind(this),
-      error: function(err) {
-        console.log("error in get request to repos. err =", err);
-      }
-    });
-  }
-
-  render() {
-    return (<div>
-        <h1>Head 2 Head Sports Gambling</h1>
-        <Games games={this.state.games}/>
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <App />, document.getElementById('root')
+)
