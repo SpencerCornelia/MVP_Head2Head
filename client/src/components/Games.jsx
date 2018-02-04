201dc79ae4b26ecd2cd0372ca6d4c6dfb6fe38cd
Spencer <spencer0cornelia@gmail.com>
2018-02-04 11:09:20 -0800

    added attributes for grabbing proper data when button is clicked

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -5,13 +5,20 @@ class Games extends React.Component {
     super(props);
   }
 
+  chooseBet(e) {
+    let gameID = e.target.getAttribute('gameid');
+    let teamID = e.target.getAttribute('teamid')
+    let wager = e.target.getAttribute('wager');
+    this.props.bet(gameID, teamID, wager);
+  }
+
   render() {
     return (<div>
       <h4>Today's Games</h4>
       {this.props.games.map((game) =>
         <div key={game._id}>
           <div>{game.away_team} vs. {game.home_team}</div>
-          <button>$20 {game.away_spread}</button>
+          <button gameid={game._id} teamid={game.away_id} wager={20} onClick={(e) => this.chooseBet(e)}>$20 {game.away_spread}</button>
           <button>$20 {game.home_spread}</button><br/>
           <button>$40 {game.away_spread}</button>
           <button>$40 {game.home_spread}</button><br/>
