44763907e40d64b3209294d5eaf3628bcfbd7967
Spencer <spencer0cornelia@gmail.com>
2018-02-04 11:10:17 -0800

    all home page functionality exists in App component

---
+++ client/src/components/App.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+import $ from 'jquery';
+import Games from './Games.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      games: []
+    }
+    this.getGames();
+  }
+
+  getGames() {
+    $.ajax({
+      url: 'http://localhost:3000/nflGames',
+      method: 'GET',
+      contentType: 'application/json',
+      success: function(data) {
+        console.log("data =", data)
+        this.setState({
+          games: data
+        });
+      }.bind(this),
+      error: function(err) {
+        console.log("error in get request to repos. err =", err);
+      }
+    });
+  }
+
+  placeBet(gameID, teamID, wager) {
+    let betData = {
+      gameID: gameID,
+      teamID: teamID,
+      wager: wager
+    };
+    $.ajax({
+      url: 'http://localhost:3000/placeUserBet',
+      data: betData,
+      method: 'POST',
+      contentType: 'application/json',
+      success: function(data) {
+
+      }.bind(this),
+      error: function(err) {
+        console.log("error in POST request to placeUserBet");
+      }
+    });
+  }
+
+  render() {
+    return (<div>
+        <h1>Head 2 Head Sports Gambling</h1>
+        <Games games={this.state.games} bet={this.placeBet.bind(this)}/>
+      </div>
+    );
+  }
+}
+
+export default App;
