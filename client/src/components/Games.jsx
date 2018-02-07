5da18dcd080aecc9a3f3dbe37676ff5dda39c5d1
Spencer <spencer0cornelia@gmail.com>
2018-02-06 22:36:48 -0800

    edited data on each button to teamName and not teamID

--- client/src/components/Games.jsx
+++ client/src/components/Games.jsx
@@ -7,9 +7,9 @@ class Games extends React.Component {
 
   chooseBet(e) {
     let gameID = e.target.getAttribute('gameid');
-    let teamID = e.target.getAttribute('teamid')
+    let teamName = e.target.getAttribute('teamname')
     let wager = e.target.getAttribute('wager');
-    this.props.bet(gameID, teamID, wager);
+    this.props.bet(gameID, teamName, wager);
   }
 
   render() {
@@ -18,14 +18,14 @@ class Games extends React.Component {
       {this.props.games.map((game) =>
         <div key={game._id}>
           <div>{game.away_team} vs. {game.home_team}</div>
-          <button gameid={game._id} teamid={game.away_id} wager={20} onClick={(e) => this.chooseBet(e)}>$20 {game.away_spread}</button>
-          <button>$20 {game.home_spread}</button><br/>
-          <button>$40 {game.away_spread}</button>
-          <button>$40 {game.home_spread}</button><br/>
-          <button>$100 {game.away_spread}</button>
-          <button>$100 {game.home_spread}</button><br/>
-          <button>$250 {game.away_spread}</button>
-          <button>$250 {game.home_spread}</button><br/>
+          <button key={1} gameid={game._id} teamname={game.away_team} wager={20} onClick={(e) => this.chooseBet(e)}>$20 {game.away_spread}</button>
+          <button key={2} gameid={game._id} teamname={game.home_team} wager={20} onClick={(e) => this.chooseBet(e)}>$20 {game.home_spread}</button><br/>
+          <button key={3} gameid={game._id} teamname={game.away_team} wager={40} onClick={(e) => this.chooseBet(e)}>$40 {game.away_spread}</button>
+          <button key={4} gameid={game._id} teamname={game.home_team} wager={40} onClick={(e) => this.chooseBet(e)}>$40 {game.home_spread}</button><br/>
+          <button key={5} gameid={game._id} teamname={game.away_team} wager={100} onClick={(e) => this.chooseBet(e)}>$100 {game.away_spread}</button>
+          <button key={6} gameid={game._id} teamname={game.home_team} wager={100} onClick={(e) => this.chooseBet(e)}>$100 {game.home_spread}</button><br/>
+          <button key={7} gameid={game._id} teamname={game.away_team} wager={250} onClick={(e) => this.chooseBet(e)}>$250 {game.away_spread}</button>
+          <button key={8} gameid={game._id} teamname={game.home_team} wager={250} onClick={(e) => this.chooseBet(e)}>$250 {game.home_spread}</button><br/>
         </div>
       )}
     </div>)
