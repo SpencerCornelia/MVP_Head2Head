049da046d434f37676df70189496b3701339b924
Spencer <spencer0cornelia@gmail.com>
2018-02-07 18:29:22 -0800

    removed bet logic

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import Bet from './Bet.jsx';
 
 class User extends Component {
   constructor(props) {
@@ -9,25 +10,12 @@ class User extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log("nextProps =", nextProps)
-    if (JSON.stringify(this.props.bets) != JSON.stringify(nextProps.bets)) {
-      this.state.bets = nextProps.bets;
-    }
-  }
-
   render() {
     return (
       <div>
         <div>UserID: {this.props.currentUser._id ? this.props.currentUser._id : 'Loading...'}</div>
         <div>Username: {this.props.currentUser.username ? this.props.currentUser.username : 'Loading...'}</div>
         <div>Bankroll: {this.props.currentUser.bankroll ? this.props.currentUser.bankroll : 'Loading...'}</div>
-        <div>Bets: {this.props.bets[0] ? this.props.bets.map((bet) => {
-          <div>
-            <br />
-            <div>GameID: {bet.gameID}</div>
-          </div>
-        }) : 'Loading...'}</div>
       </div>
     )
   }
